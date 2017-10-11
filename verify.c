3d0fd67778c4677e92d59408c39418b9392df1e6
Nicholas Kouris <nkouris@student.42.us.org>
2017-10-10 22:59:58 -0700

    broken out functions

---
+++ verify.c
@@ -0,0 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   verify.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nkouris <nkouris@student.42.us.org>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/10/10 22:41:47 by nkouris           #+#    #+#             */
+/*   Updated: 2017/10/10 22:53:57 by nkouris          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "fillit.h"
+
+static int	ff(unsigned int *shape, unsigned int pos)
+{
+	if (pos == 0 || !(*shape & pos))
+		return (0);
+	*shape ^= pos;
+	return (1
+			+ ff(shape, (pos >> 1))
+			+ ff(shape, (pos << 1))
+			+ ff(shape, (pos >> 4)));
+}
+
+t_bool		touching(t_shape *shape)
+{
+	unsigned int	data;
+	unsigned int	pos;
+
+	shape->data <<= 16;
+	data = shape->data;
+	pos = HIGH_BIT;
+	while (!(shape->data & pos))
+		pos >>= 1;
+	return (ff(&data, pos) == 4);
+}
+
+int			block_count(int shape)
+{
+	int blocks;
+
+	blocks = 0;
+	while (shape > 0)
+	{
+		if (HIGH_BIT & shape)
+			blocks++;
+		shape <<= 1;
+	}
+	return (blocks == 4);
+}
